03699cbd6562193a3f4f36ddfd0a35e1a833212a
Bamba Ibrahim <ibamba366@gmail.com>
2025-05-08 20:12:47 +0000

    Utilisation de la camere en perspective

--- Projet-3D-animation/src/main.ts
+++ Projet-3D-animation/src/main.ts
@@ -1,20 +1,14 @@
 import './style.css'
 import * as THREE from 'three'
-import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls.js'
 import Stats from 'three/examples/jsm/libs/stats.module.js'
 import { GUI } from 'dat.gui'
 
-//const scene = new THREE.Scene()
-//scene.background = new THREE.Color(0x123456)
-
-//const scene = new THREE.Scene()
-//scene.background = new THREE.TextureLoader().load('https://sbcode.net/img/grid.png')
-
 const scene = new THREE.Scene()
-scene.background = new THREE.CubeTextureLoader().setPath('https://sbcode.net/img/').load(['px.png', 'nx.png', 'py.png', 'ny.png', 'pz.png', 'nz.png'])
+scene.add(new THREE.GridHelper())
 
 const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000)
-camera.position.z = 1.5
+//camera.position.set(0, 2, 3)
+//camera.lookAt(0, 0.5, 0)
 
 const renderer = new THREE.WebGLRenderer()
 renderer.setSize(window.innerWidth, window.innerHeight)
@@ -26,12 +20,11 @@ window.addEventListener('resize', () => {
   renderer.setSize(window.innerWidth, window.innerHeight)
 })
 
-new OrbitControls(camera, renderer.domElement)
-
 const geometry = new THREE.BoxGeometry()
 const material = new THREE.MeshNormalMaterial({ wireframe: true })
 
 const cube = new THREE.Mesh(geometry, material)
+cube.position.y = 0.5
 scene.add(cube)
 
 const stats = new Stats()
@@ -39,21 +32,28 @@ document.body.appendChild(stats.dom)
 
 const gui = new GUI()
 
-const cubeFolder = gui.addFolder('Cube')
-cubeFolder.add(cube.rotation, 'x', 0, Math.PI * 2)
-cubeFolder.add(cube.rotation, 'y', 0, Math.PI * 2)
-cubeFolder.add(cube.rotation, 'z', 0, Math.PI * 2)
-cubeFolder.open()
-
 const cameraFolder = gui.addFolder('Camera')
-cameraFolder.add(camera.position, 'x', 0, 20)
+cameraFolder.add(camera.position, 'x', -10, 10)
+cameraFolder.add(camera.position, 'y', -10, 10)
+cameraFolder.add(camera.position, 'z', -10, 10)
+cameraFolder.add(camera, 'fov', 0, 180, 0.01).onChange(() => {
+  camera.updateProjectionMatrix()
+})
+cameraFolder.add(camera, 'aspect', 0.00001, 10).onChange(() => {
+  camera.updateProjectionMatrix()
+})
+cameraFolder.add(camera, 'near', 0.01, 10).onChange(() => {
+  camera.updateProjectionMatrix()
+})
+cameraFolder.add(camera, 'far', 0.01, 10).onChange(() => {
+  camera.updateProjectionMatrix()
+})
 cameraFolder.open()
 
 function animate() {
   requestAnimationFrame(animate)
 
-  cube.rotation.x += 0.01
-  cube.rotation.y += 0.01
+  //camera.lookAt(0, 0.5, 0)
 
   renderer.render(scene, camera)
 
